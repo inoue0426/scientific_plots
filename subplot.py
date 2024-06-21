c04d9e95df3b2931d67de13df07f86635abf4f7f
inoue0426 <inoue0426@users.noreply.github.com>
2024-06-21 16:15:02 +0000

    Apply auto-formatting

--- subplot.py
+++ subplot.py
@@ -1,8 +1,9 @@
-import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scienceplots
+import torch
+
 
 def plot_data(
     data,
@@ -12,16 +13,15 @@ def plot_data(
     y_label="Value",
     figsize=(10, 6),
     marker="o",
-    styles=['science', 'ieee'],
+    styles=["science", "ieee"],
     linewidth=1,
     fontsize_title=16,
     fontsize_labels=14,
     fontsize_legend=12,
-    legend_facecolor='white',
-    legend_edgecolor='black',
-    grid=True
+    legend_facecolor="white",
+    legend_edgecolor="black",
+    grid=True,
 ):
-
     """
     Function to plot data.
     You can specify parameters such as 'linewidth', etc.
@@ -30,19 +30,49 @@ def plot_data(
     plt.figure(figsize=figsize)
 
     # Select appropriate plotting method based on data type and automatically change markers
-    markers = ['o', 's', '^', 'D', '*', 'x', 'p', 'h', '+', '>', '<', '|', '_']  # Expanded list of available markers
+    markers = [
+        "o",
+        "s",
+        "^",
+        "D",
+        "*",
+        "x",
+        "p",
+        "h",
+        "+",
+        ">",
+        "<",
+        "|",
+        "_",
+    ]  # Expanded list of available markers
     if isinstance(data, pd.DataFrame):
         for idx, (column, lbl) in enumerate(zip(data.columns, label)):
-            plt.plot(data.index, data[column], marker=markers[idx % len(markers)], label=f"{lbl} - {column}", linewidth=linewidth)
+            plt.plot(
+                data.index,
+                data[column],
+                marker=markers[idx % len(markers)],
+                label=f"{lbl} - {column}",
+                linewidth=linewidth,
+            )
     elif isinstance(data, np.ndarray):
         if data.ndim == 1:
             plt.plot(data, marker=markers[0], label=label, linewidth=linewidth)
         elif data.ndim == 2:
             for i, lbl in enumerate(label):
-                plt.plot(data[:, i], marker=markers[i % len(markers)], label=lbl, linewidth=linewidth)
+                plt.plot(
+                    data[:, i],
+                    marker=markers[i % len(markers)],
+                    label=lbl,
+                    linewidth=linewidth,
+                )
     else:  # Assuming this is a list of lists (Array of Arrays)
         for idx, (sub_data, lbl) in enumerate(zip(data, label)):
-            plt.plot(sub_data, marker=markers[idx % len(markers)], label=lbl, linewidth=linewidth)
+            plt.plot(
+                sub_data,
+                marker=markers[idx % len(markers)],
+                label=lbl,
+                linewidth=linewidth,
+            )
 
     plt.title(title, fontsize=fontsize_title)
     plt.xlabel(x_label, fontsize=fontsize_labels)
